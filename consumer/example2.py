dbc91c2e7176a6a62ae24449b0b2b75aa213e960
Bawon <davi-kim@hotmail.com>
2022-12-12 00:08:12 +0100

    Update example2.py

--- consumer/example2.py
+++ consumer/example2.py
@@ -7,7 +7,7 @@ consumer = KafkaConsumer(
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
-     group_id='group1',
+     group_id='test',
      api_version=(0,10,2),
      value_deserializer=lambda x: x.decode('utf-8'))
 
