1b53c7c1df5a444ca1e438515c0646f87d60ed12
Bawon <davi-kim@hotmail.com>
2022-12-12 17:26:56 +0100

    Update pysparkFileWrite.py

--- clusterPyspark/pysparkFileWrite.py
+++ clusterPyspark/pysparkFileWrite.py
@@ -1,14 +1,19 @@
-from pyspark import SparkConf, SparkContext
+#from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
 import locale
 locale.getdefaultlocale()
 locale.getpreferredencoding()
 import time
-
+spark = SparkSession \
+        .builder \
+        .appName("myAppName") \
+        .config("spark.sql.debug.maxToStringFields", "100") \
+        .getOrCreate()
 
 
 # Limit cores to 1, and tell each executor to use one core = only one executor is used by Spark
-conf = SparkConf().set('spark.executor.cores', 1).set('spark.cores.max',1).set('spark.executor.memory', '1g')
-sc = SparkContext(master='spark://spark-master:7077', appName='myAppName', conf=conf)
+#conf = SparkConf().set('spark.executor.cores', 1).set('spark.cores.max',1).set('spark.executor.memory', '1g')
+#sc = SparkContext(master='spark://spark-master:7077', appName='myAppName', conf=conf)
 
 df = spark \
   .read \
